b1411c1464bee0ea5572eccfe9577a025240dfae
Jesus Betancourt <jbeta02@gmail.com>
2026-03-20 22:28:11 -0700

    modify architecture of graph. add working tool usage. add persistant memory. add graph display

--- scripts/robot_head.py
+++ scripts/robot_head.py
@@ -1,13 +1,33 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+
 from typing import TypedDict
+from typing import Annotated
 from dotenv import load_dotenv
-import os
-from dummy_speak import DummySpeak
+
 
 ############################# setup llm ###################################
 load_dotenv()
-llm = ChatOpenAI(model="gpt-4o-mini")
+
+@tool
+def getWeather(location:str) -> str:
+    """
+    return tempature of location in degrees Fahrenheit
+    :param location: city state
+    :return: degrees Fahrenheit
+    """
+
+    print("running tool")
+
+    return f"80.0 degrees F in {location}"
+
+tools = [getWeather]
+llm = ChatOpenAI(model="gpt-4o-mini").bind_tools(tools)
+memory = MemorySaver()
 
 ############################# setup global variables ###################################
 robot_name = "Candle"
@@ -29,52 +49,34 @@ life_goal = f"""
     the user says your name followed by their prompt. For example, "{robot_name} what 
     is one plus one". 
 
-    If you think it will help you may choose to move your head and body to emphasize 
-    your point. 
-
     Below or instructions on your input and output options. 
 """
-
-speak_action = DummySpeak()
-# speak_action2 = DummySpeak()
-# speak_action3 = DummySpeak()
-
-actions = []
-actions.append(speak_action)
-# actions.append(speak_action2)
-# actions.append(speak_action3)
 
 class RobotState(TypedDict):
     # captured observations
     audio: str
     vision: str
 
-    # action to be perormed
-    action: str
-
-    # action performed
-    response: str
+    # message in message format
+    messages: Annotated[list, add_messages]
 
 ############################# create nodes ###################################
-def sense(state: RobotState):
+def sense(state: RobotState): # TODO: add delay and only check environment periodically, potentially in prompt allow llm to be idle
     print("[SENSE] running")
     audio_result = input("audio: ") # "ok"
     vision_result = input("vision: ") # "I see a person at a desk using their phone and computer." "individual is working at desk"
     
     return {
         "audio": audio_result,
-        "vision": vision_result
+        "vision": vision_result,
+        "messages": [
+            {"role": "user", "content": audio_result}
+        ]
     }
 
 
 def think(state: RobotState):
     print("[THINK] running")
-
-    # build action options string
-    list_actions = []
-    for action in actions:
-        list_actions.append(f"- {action.name}:{action.type}\n")
-    actions_string = "".join(list_actions)
 
     prompt = f"""
 
@@ -84,37 +86,30 @@ def think(state: RobotState):
     What you observed: {state['vision']}
     
     Decide what to do next.
-    Choose ONE action option:
-    {actions_string}
-    - none
-    
-    Respond in format: [action]: [text]
-    Where [action] is one of the options and [text] should contain
-    no special characters so that it can be easily read out load.
 
-    Example: speak: Try not to get distracted.
-    Example: none
+    Either use a tool or communicate with user.
     """
 
-    result = llm.invoke(prompt).content
+    # result = llm.invoke(prompt + str(state["messages"]))
+    messages = [
+        {"role": "user", "content": prompt},
+        *state["messages"],
+    ]
 
-    print("[THINK] prompt:", prompt)
-    print("[THINK] raw LLM output:", result)
-    return {"action": result}
+    result = llm.invoke(messages)
+
+    print("[THINK] prompt:", prompt + str(state["messages"]))
+    print("[THINK] raw LLM output:", result.content)
+    return {"messages": [result]}
 
 
-def act(state: RobotState):
+def act(state: RobotState): # will use this node to speak only!
     print("[ACT] running")
-    action = state["action"]
-    
-    for action_option in actions:
-        if action.startswith(action_option.name + ":"):
-            response = action.replace(action_option.name + ":", "")
-            print(f"[ACT] {action_option.tag}{response}")
-            return {"action": response}
 
-    print("[ACT] No valid action detected")
-    return {"response": "Idle"}
+    last_msg = state["messages"][-1]
+    if hasattr(last_msg, "content") and last_msg.content:
+        print("[ACT] speaking:", last_msg.content)
+    return {}
 
 
 
@@ -123,21 +118,40 @@ graph = StateGraph(RobotState)
 
 graph.add_node("sense", sense)
 graph.add_node("think", think)
+graph.add_node("tools", ToolNode(tools))
 graph.add_node("act", act)
 
 graph.set_entry_point("sense")
 
 graph.add_edge("sense", "think")
-graph.add_edge("think", "act")
+graph.add_conditional_edges(
+    "think",
+    tools_condition,
+    {
+        "tools": "tools",
+        "__end__": "act",
+    }
+)
+graph.add_edge("tools", "think")
 graph.add_edge("act", "sense")
 
-app = graph.compile()
+app = graph.compile(checkpointer=memory)
+
+png_data = app.get_graph().draw_mermaid_png()
+with open("graph_visualization.png", "wb") as f:
+    f.write(png_data)
+
 
 state = {
     "audio": "",
     "vision": "",
-    "action": "",
-    "response": "",
+    "messages": [{"role": "user",
+                   "content": f"""
+                                Your name is "{robot_name}". Your is goal is to act as a robot companion that will 
+                                sit on the user's desk and help them be productive.
+                                """
+                }],
 }
 
-result = app.invoke(state)
+config1 = { 'configurable': { 'thread_id': '1'} }
+result = app.invoke(state, config=config1)
